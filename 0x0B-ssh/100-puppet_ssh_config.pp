03bf4da971736f4cc83f99a278947c1139d5b041
SarahEmmy <bawahsarah@gmail.com>
2023-09-24 18:23:35 -0700

    updates using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,10 @@
-#!/usr/bin/env bash
-# Client configuration file using Puppet
-file { 'etc/ssh/ssh_config':
-	ensure => present,
+#!/usr/bin/env puppet
 
-content =>"
-
-	# SSH client configuration
-	host *
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "# SSH client configuration
+Host *
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+",
 }
